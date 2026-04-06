d38c9e9e03b169aaa2780c667b80073b0b9d68a9
Anand jha <anand.jha@gotrust.tech>
2026-04-06 18:23:52 +0530

    last commit of the day

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ const app = express();
 
 
   const pool = new Pool({
-  host: process.env.DB_HOST,   
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString:process.env.DATABASE_URL
   });
 
   const connectWithRetry = async () => {
